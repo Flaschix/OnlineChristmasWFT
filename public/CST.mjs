796546297b8f2e3723afc585a165d6e09c59630d
Flaschix <mishasigalov1@gmail.com>
2024-10-23 13:38:36 +0300

    logo upd and text

--- public/CST.mjs
+++ public/CST.mjs
@@ -1,1 +1,19 @@
-export const CST = { SCENE: { LOADINGSCENE: "LoadingScene", LOBBYSCENE: "LobbyScene", GAMESCENE: "GameScene", GAMESCENE2: "GameScene2", GAMESCENE3: "GameScene3", GAMESCENE4: "GameScene4", GAMESCENE5: "GameScene5", GAMESCENE6: "GameScene6" } }; export const socket = io(); export const LABEL_ID = { DOOR_FORWARD_ID: 11111111, DOOR_BACK_ID: 22222222, FIRST_KEY: 33333333, SECOND_KEY: 44444444, FIRST_JOKE: 55555555, SECOND_JOKE: 6666666 }; export const myMap = new Map([["newyorkKey1", { x: 445, y: 300, text: "Ргы ъгфсесм тсвф\nсхфхгих сх\nефиплурсжс еуипирл\nрг 5 ъгфсе." }], ["newyorkKey2", { x: 445, y: 330, text: "Фимъгф е Ряб-Мсуни\n17:00 (5:00 еиъиуг)." }], ["parisKey1", { x: 430, y: 310, text: "Ргы ъгфсесм тсвф\nефижс рг 1 ъгф етиуизл\nефиплурсжс еуипирл." }], ["parisKey2", { x: 420, y: 330, text: "Е Тгулйи фимъгф 12:00\n(тсозиря)." }], ["pekinKey1", { x: 440, y: 280, text: "Пю ргшсзлпфв е\nъгфсесп тсвфи,\nнсхсуюм рг 8 ъгфсе\nетиуизл ефиплурсжс\nеуипирл." }], ["pekinKey2", { x: 415, y: 330, text: "Фимъгф е ргыип жсусзи\n8:00 цхуг." }], ["rioKey1", { x: 435, y: 320, text: "Пю схфхгип сх\nефиплурсжс еуипирл\nрг 3 ъгфг." }], ["rioKey2", { x: 450, y: 320, text: "Фимъгф е Улс-зи-\nЙгримус 16:00 (4:00\nзрв)." }], ["sidneyKey1", { x: 425, y: 300, text: "Пю ргшсзлпфв е\nъгфсесп тсвфи,\nнсхсуюм стиуийгих\nефиплурси еуипв рг 11\nъгфсе." }], ["sidneyKey2", { x: 415, y: 330, text: "Fимъгф е ргыип жсусзи\n14:00 (2:00 зрв)." }], ["tokioKey1", { x: 420, y: 320, text: "Ргы ъгфсесм тсвф рг 9\nъгфсе етиуизл\nефиплурсжс еуипирл." }], ["tokioKey2", { x: 410, y: 330, text: "Е ргыип жсусзи фимъгф\n9:00 цхуг." }]]);
+export const CST = { SCENE: { LOADINGSCENE: "LoadingScene", LOBBYSCENE: "LobbyScene", GAMESCENE: "GameScene", GAMESCENE2: "GameScene2", GAMESCENE3: "GameScene3", GAMESCENE4: "GameScene4", GAMESCENE5: "GameScene5", GAMESCENE6: "GameScene6" } };
+export const socket = io();
+
+export const LABEL_ID = { DOOR_FORWARD_ID: 11111111, DOOR_BACK_ID: 22222222, FIRST_KEY: 33333333, SECOND_KEY: 44444444, FIRST_JOKE: 55555555, SECOND_JOKE: 6666666 };
+
+export const myMap = new Map([
+    ["newyorkKey1", { x: 430, y: 320, text: "Rxu wlph crqh lv 5 krxuv\nehklqg zruog wlph." }],
+    ["newyorkKey2", { x: 440, y: 330, text: "Lw lv fxuuhqwob 5:00 SP\nlq Qhz Brun." }],
+    ["parisKey1", { x: 430, y: 310, text: "Rxu wlph crqh lv rqob 1\nkrxu dkhdg ri zruog\nwlph." }],
+    ["parisKey2", { x: 440, y: 330, text: "Lw lv fxuuhqwob 12:00 SP\n(qrrq) lq Sdulv." }],
+    ["pekinKey1", { x: 430, y: 300, text: "Zh duh lq d wlph crqh\nwkdw lv 8 krxuv dkhdg ri\nzruog wlph." }],
+    ["pekinKey2", { x: 430, y: 330, text: "Lw lv fxuuhqwob 8:00 DP\nlq rxu flwb." }],
+    ["rioKey1", { x: 435, y: 320, text: "Zh duh 3 krxuv ehklqg\nzruog wlph." }],
+    ["rioKey2", { x: 440, y: 320, text: "Lw lv fxuuhqwob 4:00 SP\nlq Ulr gh Mdqhlur." }],
+    ["sidneyKey1", { x: 425, y: 300, text: "Zh duh lq d wlph crqh\nwkdw lv 11 krxuv dkhdg ri\nzruog lph." }],
+    ["sidneyKey2", { x: 430, y: 330, text: "Lw lv fxuuhqwob 2:00 SP\nlq rxu flwb." }],
+    ["tokioKey1", { x: 420, y: 320, text: "Rxu wlph crqh lv 9 krxuv\ndkhdgri zruog wlph." }],
+    ["tokioKey2", { x: 430, y: 330, text: "Lw lv fxuuhqwob 9:00 DP\nlq rxu flwb." }]
+]);
